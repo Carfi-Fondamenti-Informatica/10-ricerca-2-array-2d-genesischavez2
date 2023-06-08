ae815d73e4b26e2f301b5801c5cf0e38fb4c9267
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-06-08 22:57:16 +0200

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
-
+void init (char mat[], int n);
+void init(char mat[5][15]);
+void inserimento(char nomi[5][15], char cognomi[5][15]);
+void inserimento(char mat[],int nomi );
+int  ricerca( char nomi[5][15], char cognomi[5][15],char nome[], char cognome[] );
