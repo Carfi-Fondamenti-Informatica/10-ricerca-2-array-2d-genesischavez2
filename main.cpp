9a842434b8b4e9b40ba303a0dbd5c578b57ddf75
genesischavez2 <127029593+genesischavez2@users.noreply.github.com>
2023-06-08 22:53:59 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  return 0;
+int main() {
+    char nomi[5][15], cognomi[5][15];
+    init(nomi);
+    init(cognomi);
+    inserimento(nomi, cognomi);
+    char nome[15], cognome[15];
+    inserimento(nome, 15);
+    inserimento(cognome, 15);
+    int id = ricerca(nomi, cognomi, nome, cognome);
+    if (id != -1) {
+        cout << "trovato in posizione " << id << endl;
+    }else{
+        cout << "non trovato" <<endl;
+    }
+    return 0;
 }
